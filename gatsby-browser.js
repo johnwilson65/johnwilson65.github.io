ac16a31bfb8f7901943abfdb4677ea5b382b6959
designspin <admin@designspin.co.uk>
2018-06-21 10:15:59 +0100

    Custom event polyfill for IE

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,21 @@ import createHistory from "history/createBrowserHistory"
 
 import getTransitionStyle from "./src/util/getTransitionStyle"
 
+(function () {
+  if ( typeof window.CustomEvent === "function" ) return false; //If not IE
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
 const timeout = 250
 const historyExitingEventType = `history::exiting`
 
